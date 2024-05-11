56b1bfc7bba1f6db6681e001b7a2385cffdb3796
Hamed Hosseini <hamed8080@gmail.com>
2024-05-11 09:41:33 +0330

    Version: Bump up the version to 2.0.0 for new release

--- Async.podspec
+++ Async.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "Async"
-  s.version      = "1.3.1"
+  s.version      = "2.0.0"
   s.summary      = "Async"
   s.description  = "Async SDk manage all communication with Chat Server."
   s.homepage     = "https://pubgi.fanapsoft.ir/chat/ios/async"
@@ -12,6 +12,6 @@ Pod::Spec.new do |s|
   s.source_files = "Sources/Async/**/*.{h,swift,m}"
   s.framework  = "Foundation"
   s.dependency "Starscream"  , '~> 3.1.1'
-  s.dependency "Additive" , '~> 1.0.1'
-  s.dependency "Logger" , '~> 1.0.2'
+  s.dependency "Additive" , '~> 1.2.0'
+  s.dependency "Logger" , '~> 1.2.0'
 end
--- Package.swift
+++ Package.swift
@@ -13,23 +13,23 @@ let package = Package(
         .library(name: "Async", targets: ["Async"]),
     ],
     dependencies: [
-        .package(path: "../Logger"),
-        .package(path: "../Mocks"),
-        .package(path: "../Additive"),
+        .package(url: "https://pubgi.fanapsoft.ir/chat/ios/logger", from: "1.2.0"),
+        .package(url: "https://pubgi.fanapsoft.ir/chat/ios/mocks", from: "1.2.0"),
+        .package(url: "https://pubgi.fanapsoft.ir/chat/ios/additive", from: "1.2.0"),
         .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMajor(from: "3.1.1")),
         .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"),
     ],
     targets: [
         .target(name: "Async", dependencies: [
             "Starscream",
-            "Logger",
             .product(name: "Additive", package: "additive"),
+            .product(name: "Logger", package: "logger"),
         ]),
         .testTarget(name: "AsyncTests",
                     dependencies: [
-                        "Async",
-                        "Logger",
-                        "Mocks",
+                        .product(name: "Additive", package: "additive"),
+                        .product(name: "Mocks", package: "mocks"),
+                        .product(name: "Logger", package: "logger"),
                     ],
                     path: "Tests"),
     ]
